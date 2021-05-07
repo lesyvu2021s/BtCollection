462a69aa9f1b243eb010e67a94044d7d8c05ae8d
___Dxing___ <lesyvu07@gmail.com>
2021-05-07 22:15:21 +0700

    update bt java

--- BtLambdaExpression/src/main/java/com/mycompany/btlambdaexpression/Addable.java
+++ BtLambdaExpression/src/main/java/com/mycompany/btlambdaexpression/Addable.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.interfacebt;
+package com.mycompany.btlambdaexpression;
 
 /**
  *
  * @author techcare
  */
-public interface TwoTwo {
-    void print();
+@FunctionalInterface
+public interface Addable {
+    int add(int a, int b);
 }
+
--- CollectionExample/src/main/java/com/mycompany/collectionexample/HashMapExample.java
+++ CollectionExample/src/main/java/com/mycompany/collectionexample/HashMapExample.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.interfacebt;
+package com.mycompany.collectionexample;
 
 /**
  *
  * @author techcare
  */
-public interface FourFour extends OneOne{
-    void show();
+public class HashMapExample {
+    
 }
